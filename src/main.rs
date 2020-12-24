07ac169da404e35786eb799511a7d24492a85ec6
Alicja Kluczek <alicja2602@gmail.com>
2020-12-24 03:22:14 +0100

    Set up tera and rocket

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+#[macro_use] extern crate rocket;
+use std::collections::HashMap;
+use rocket_contrib::templates::Template;
+use rocket_contrib::serve::StaticFiles;
+
+#[get("/")]
+fn index() -> Template {
+    let context: HashMap<&str, &str> = HashMap::new();
+    Template::render("index", &context)
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite()
+        .mount("/", routes![index])
+        .mount("/static", StaticFiles::from("static"))
+        .attach(Template::fairing())
+        .launch();
 }
